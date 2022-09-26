348cf3015e3caebaba18765bd93cafc87ba17366
nicoelias997 <nicoelias997@gmail.com>
2022-09-26 10:31:01 +0200

    Elimino los archivos que no usare, limpio el App, y creo una carpeta components

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="container mt-5">
+      <h1>
+        TP8 - Ejercicio 4
+      </h1>
+    </div>
+  );
+}
+
+export default App;
