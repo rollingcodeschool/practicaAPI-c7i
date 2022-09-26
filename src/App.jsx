4453ed0b2b75310bca1d031d081dbeaae540ec21
nicoelias997 <nicoelias997@gmail.com>
2022-09-26 12:42:17 +0200

    creamos el componente Frase, y damos estilo a nuestro App principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import React from "react";
 
+import Frase from "./components/Frase";
+
 function App() {
   return (
-    <div className="container mt-5">
-      <h1>
-        TP8 - Ejercicio 4
-      </h1>
+    <div className="card bg-danger border border-secondary">
+      <div className="container">
+      <div className="d-flex justify-content-center align-content-center row">
+        <div className="card-img-top d-flex justify-content-center">
+          <img src="https://trello.com/1/cards/63288a1289c67400fd5b534f/attachments/632b24ad1ed8b700597e4050/previews/632b24ae1ed8b700597e418b/download/theSimpson.png" style={{width:"80%"}} className="p-3" alt="Los simpsons titulo" />
+        </div>
+          <button type="button" className="btn btn-warning text-dark btn-sm col-3 mt-5 mb-5">
+            Obtener frase
+          </button>
+        </div>
+      <div className="card-body">
+        <Frase></Frase>
+      </div>
     </div>
+    </div>
+
   );
 }
 
---
+++ src/components/Frase.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Frase = () => {
+  return (
+    <div className='container'>
+        <div className="card bg-white">          
+            <div className="d-flex justify-content-center">
+
+           <div className="col-3 p-3">
+            <img src="https://trello.com/1/cards/63288a1289c67400fd5b534f/attachments/632b248698baf80018c2e999/previews/632b248698baf80018c2e99f/download/image.png" alt="Homero Simpsons" />
+           </div>
+
+        <div className="col-7 container p-3">
+            <div className="card-title">
+                <h5>Homero Simpsons</h5>
+            </div>
+            <div className="card-text">
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius voluptate amet beatae tenetur, ullam neque qui odio tempore possimus fugiat, quia consequatur alias iure, veniam soluta atque cum exercitationem at.</p> 
+            </div>
+            </div>
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default Frase
