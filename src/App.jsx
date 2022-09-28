e7c8be1a3a67ff9826cea77ed0c05248d5b5f0a4
Emilse Arias <emilsearias@gmail.com>
2022-09-28 11:01:34 -0300

    agregamos detalles esteticos al fondo de pantalla

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,54 @@
 import { useEffect, useState } from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import './app.css';
 import Frase from "./components/Frase";
+import { Button, Container } from "react-bootstrap";
 
 function App() {
   const [personaje, setPersonaje] = useState({});
 
-  useEffect(()=>{
+  useEffect(() => {
     // la consulta de la api solo sucede en el montaje
     consultarAPI();
-  },[])
+  }, []);
 
-  const consultarAPI = async ()=>{
+  const consultarAPI = async () => {
     try {
       // aqui hago la peticion o request
-      const respuesta = await fetch('https://thesimpsonsquoteapi.glitch.me/quotes')
+      const respuesta = await fetch(
+        "https://thesimpsonsquoteapi.glitch.me/quotes"
+      );
       console.log(respuesta);
       const dato = await respuesta.json();
-      console.log(dato[0])
+      console.log(dato[0]);
       //guardar la frase del personaje en el state
       setPersonaje(dato[0]);
-
     } catch (error) {
-      console.log(error)
+      console.log(error);
       // mostrar un mensaje de error al usuario
     }
-  }
-
+  };
 
   return (
-    <div className="card bg-danger border border-secondary">
-      <div className="container">
-        <div className="d-flex justify-content-center align-content-center row">
-          <div className="card-img-top d-flex justify-content-center">
-            <img
-              src="https://trello.com/1/cards/63288a1289c67400fd5b534f/attachments/632b24ad1ed8b700597e4050/previews/632b24ae1ed8b700597e418b/download/theSimpson.png"
-              style={{ width: "80%" }}
-              className="p-3"
-              alt="Los simpsons titulo"
-            />
-          </div>
-          <button
-            type="button"
-            className="btn btn-warning text-dark btn-sm col-3 mt-5 mb-5"
-          >
-            Obtener frase
-          </button>
+    <Container>
+      <div className="d-flex justify-content-center align-content-center row">
+        <div className="card-img-top d-flex justify-content-center">
+          <img
+            src="https://trello.com/1/cards/63288a1289c67400fd5b534f/attachments/632b24ad1ed8b700597e4050/previews/632b24ae1ed8b700597e418b/download/theSimpson.png"
+            style={{ width: "80%" }}
+            className="p-3"
+            alt="Los simpsons titulo"
+          />
         </div>
-        <div className="card-body">
-          <Frase personaje={personaje}></Frase>
-        </div>
+        {/* className="btn btn-warning text-dark btn-sm col-3 mt-5 mb-5" */}
+        <Button type="button" variant={"warning"} className="w-50 mb-5">
+          Obtener frase
+        </Button>
       </div>
-    </div>
+      <div className="card-body">
+        <Frase personaje={personaje}></Frase>
+      </div>
+    </Container>
   );
 }
 
--- src/components/Frase.jsx
+++ src/components/Frase.jsx
@@ -1,6 +1,6 @@
 import { Card, Container, Row, Col } from "react-bootstrap";
 
-const Frase = () => {
+const Frase = ({personaje}) => {
   return (
     <>
       <Container>
@@ -8,21 +8,15 @@ const Frase = () => {
           <Card.Body>
           <Row>
             <Col md={5} className='text-center'>
-              <img
-                src="https://trello.com/1/cards/63288a1289c67400fd5b534f/attachments/632b248698baf80018c2e999/previews/632b248698baf80018c2e99f/download/image.png"
-                alt="Homero Simpsons"
+              <img src={personaje.image} alt={personaje.character}
               />
             </Col>
             <Col md={7}>
               <Card.Title>
-                <h5>Homero Simpsons</h5>
+                <h5>{personaje.character}</h5>
               </Card.Title>
               <Card.Text>  
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius
-                  voluptate amet beatae tenetur, ullam neque qui odio tempore
-                  possimus fugiat, quia consequatur alias iure, veniam soluta
-                  atque cum exercitationem at.
-              
+                 {personaje.quote}
               </Card.Text>
             </Col>
           </Row>
